57cc1a7c03916e382b497fa68181b3167078921e
aamirhannan <aamirhannan08@gmail.com>
2024-01-27 17:49:00 +0530

    util code discussion

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// Import section
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+// Middleware section
+const app = express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json()); //this is needed for express json parsing
+app.use(express.urlencoded()); //this is needed for express url, different url have different meanings but it helps to pars all URLs uniformally
+app.use(express.static("public")) //this is needed for express static, It will fetch static file stored in public folders
+app.use(cookieParser()) //this is needed for handling cookies, it will inject and fetch user cookies securely
+
+// exports module
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
+import {app} from "./app.js"
 
 dotenv.config({path:'../.env'})
 
+const PORT = process.env.PORT||8000;
 
 connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`server running on port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('Database connection failed',err)
+})
 
 /*
 // Approach 1 to connect to Mongo
